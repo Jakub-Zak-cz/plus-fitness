41341df63630e3876b5359319e491e2f2adfa52e
Jakub-Zak-cz <coder.jakub.zak@icloud.com>
2023-07-28 14:28:32 +0200

    hero section, why-us section, about-us section + css + responzivita

--- header.php
+++ header.php
@@ -7,3 +7,43 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<header id="primary-header" class="shadow" >
+    <div class="nav-logo">
+            <a href="<?php echo home_url(); ?>">
+                <?php if (has_custom_logo()) {
+                    the_custom_logo();
+                } ?>
+            </a>
+    </div>
+
+    <nav aria-label="primary-navigation" class="primary-navigation">
+        <ul>
+            <?php
+            // We get navigation menu items named 'primary-navigation'
+            $menu_items = wp_get_nav_menu_items('primary-navigation');
+
+            // List a link for each menu item
+            if ($menu_items) {
+                foreach ($menu_items as $menu_item) {
+                    $is_current = false;
+                    if (is_page($menu_item->object_id)) {
+                        $is_current = true;
+                    }
+
+                    echo '<li' . ($is_current ? ' class="current_page_item"' : '') . '>';
+                    echo '<a href="' . $menu_item->url . '" ' . ($is_current ? 'aria-current="page"' : '') . '>' . $menu_item->title . '</a>';
+                    echo '</li>';
+                }
+            }
+
+            ?>
+
+            <li class="nav_footer-link"><a href="#footer-nav">Kontakt</a></li>
+
+        </ul>
+    </nav>
+
+</header>
+
+
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 <main>
-    <!-- content -->
+<?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                the_content();
+            endwhile;
+        endif;
+?>
 </main>
 <?php get_footer(); ?>

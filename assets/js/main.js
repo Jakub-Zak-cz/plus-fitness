98dac9a70179fb93c2a805cac01fa7e1f60396f6
Jakub-Zak-cz <coder.jakub.zak@icloud.com>
2023-07-30 18:23:23 +0200

    sekce s recenzemi, sekce s galerii, + responsivni + showcase file

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,29 @@ var swiper = new Swiper(".trainers", {
         },
     },
 });
+
+ var reviews_slider = new Swiper(".reviews_slider", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+var gallery_slider = new Swiper(".gallery-slider", {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   breakpoints: {
+     900: {
+         slidesPerView: 2,
+     },
+   }  
+ });
--- functions.php
+++ functions.php
@@ -117,6 +117,116 @@ function custom_theme_register_customizer_sections($wp_customize) {
 }
 add_action('customize_register', 'custom_theme_register_customizer_sections');
 
+/** reviews */
+function customizer_fitness_reviews_section($wp_customize) {
+    // Vytvoření nové sekce
+    $wp_customize->add_section('custom_fitness_reviews', array(
+        'title' => 'Recenze', // Název sekce
+        'description' => 'Přidání recenzí o vašem fitness centru.', // Popis sekce
+        'priority' => 150, // Priorita sekce
+    ));
+
+    // Přidání možnosti pro nastavení počtu recenzí
+    $wp_customize->add_setting('custom_reviews_count', array(
+        'default' => 3, // Počet recenzí jako výchozí hodnota
+        'sanitize_callback' => 'absint', // Očistí hodnotu a zajistí, že bude celé číslo
+    ));
+
+    $wp_customize->add_control('custom_reviews_count', array(
+        'label' => 'Počet recenzí k zobrazení', // Název pole
+        'section' => 'custom_fitness_reviews', // Přiřazení k nové sekci
+        'type' => 'number',
+    ));
+
+    // Loop přes počet recenzí podle nastaveného počtu z customizeru
+    $count = get_theme_mod('custom_reviews_count', 3);
+    for ($i = 1; $i <= $count; $i++) {
+        
+        $wp_customize->add_setting("custom_review_image_$i", array(
+            'sanitize_callback' => 'esc_url_raw', // Očistí URL
+        ));
+
+        $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, "custom_review_image_$i", array(
+            'label' => "Obrázek recenze $i", // Název pole
+            'section' => 'custom_fitness_reviews', // Přiřazení k nové sekci
+            'settings' => "custom_review_image_$i", // Přiřazení k nastavení
+        )));
+
+        $wp_customize->add_setting("custom_review_firstname_$i", array(
+            'sanitize_callback' => 'sanitize_text_field', // Očistí textové pole od nežádoucích znaků
+        ));
+
+        $wp_customize->add_control("custom_review_firstname_$i", array(
+            'label' => "Křestní jméno recenze $i", // Název pole
+            'section' => 'custom_fitness_reviews', // Přiřazení k nové sekci
+            'type' => 'text',
+        ));
+
+        $wp_customize->add_setting("custom_review_lastname_$i", array(
+            'sanitize_callback' => 'sanitize_text_field', // Očistí textové pole od nežádoucích znaků
+        ));
+
+        $wp_customize->add_control("custom_review_lastname_$i", array(
+            'label' => "Příjmení recenze $i", // Název pole
+            'section' => 'custom_fitness_reviews', // Přiřazení k nové sekci
+            'type' => 'text',
+        ));
+
+        $wp_customize->add_setting("custom_review_text_$i", array(
+            'sanitize_callback' => 'sanitize_textarea_field', // Očistí textovou oblast od nežádoucích znaků
+        ));
+
+        $wp_customize->add_control("custom_review_text_$i", array(
+            'label' => "Text recenze $i", // Název pole
+            'section' => 'custom_fitness_reviews', // Přiřazení k nové sekci
+            'type' => 'textarea',
+        ));
+    }
+}
+add_action('customize_register', 'customizer_fitness_reviews_section');
+
+
+/** gallery */
+function fitness_customizer_settings($wp_customize) {
+    // Sekce Galerie
+    $wp_customize->add_section('fitness_gallery', array(
+        'title' => __('Galerie', 'fitness'),
+        'priority' => 30,
+    ));
+
+    // Počet obrázků v galerii
+    $wp_customize->add_setting('fitness_gallery_count', array(
+        'default' => 3,
+        'sanitize_callback' => 'absint', // Očistí hodnotu a převede ji na celé číslo
+    ));
+
+    $wp_customize->add_control('fitness_gallery_count', array(
+        'label' => __('Počet obrázků v galerii', 'fitness'),
+        'section' => 'fitness_gallery',
+        'type' => 'number',
+        'input_attrs' => array(
+            'min' => 1,
+            'max' => 10, // Můžete upravit maximální počet obrázků, pokud chcete
+        ),
+    ));
+
+    // Pole pro každý obrázek v galerii
+    for ($i = 1; $i <= 10; $i++) { // Uvádíme maximální počet obrázků, který může být v galerii (zde 10)
+        $wp_customize->add_setting("fitness_gallery_image_$i", array(
+            'default' => '',
+            'sanitize_callback' => 'esc_url_raw', // Očistí URL obrázku
+        ));
+
+        $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, "fitness_gallery_image_$i", array(
+            'label' => sprintf(__('Obrázek %s', 'fitness'), $i),
+            'section' => 'fitness_gallery',
+        )));
+    }
+}
+
+add_action('customize_register', 'fitness_customizer_settings');
+
+
 /** shortcode */
 // Funkce pro vytvoření shortcode
 function fitness_trainers_slider_shortcode() {
@@ -126,7 +236,7 @@ function fitness_trainers_slider_shortcode() {
     <section aria-label="Trenéři +fitness" class="custom-trainers">
         <header class="container">
             
-            <h2 class="custom-trainers_title">Naši <b>Trenéři</b></h2>
+            <h2 class="custom-trainers_title">Naši <b class="accent-text">Trenéři</b></h2>
 
             <span class="custom-trainers_undertitle">S našimi trenéry dosáhnete svých cílů s jistotou a profesionálním vedením.</span>
 
@@ -200,3 +310,122 @@ function fitness_trainers_slider_shortcode() {
     return ob_get_clean(); // Vrácení obsahu vyrovnávací paměti jako výstup z shortcode
 }
 add_shortcode('fitness_trainers_slider', 'fitness_trainers_slider_shortcode');
+
+
+/** reviews shortcode */
+function fitness_reviews_shortcode() {
+    ob_start(); 
+    ?>
+
+    <aside class="reviews" aria-label="recenze" title="reviews">
+        <div class="container">
+            <header class="reviews-introduction" >
+                
+                <h2 class="reviews_title">Co o nás říkají <b class="accent-text">naši zákazníci</b></h2>
+            
+                <p class="reviews_paragraph">Níže najdete několik referencí od našich klientů, kteří nám důvěřovali a dosáhli skvělých výsledků. Jejich příběhy jsou důkazem naší oddanosti a schopnosti pomoci lidem dosáhnout jejich fitness cílů. Přečtěte si jejich zkušenosti a motivujte se k vlastnímu úspěchu. Recenze jsou přímo z našeho <a href="https://www.facebook.com/plusfitko/reviews" target="_blank" aria-label="Odkaz na sekci s recenzemi z naší Facebook stránky."> Facebooku</a></p>
+            
+            </header>
+            <div class="reviews_slider swiper">
+
+                <div class="swiper-wrapper reviews-wrapper">
+
+        <?php 
+
+            $count = get_theme_mod('custom_reviews_count', 3); // Získáme nastavený počet recenzí z customizeru
+
+            for ($i = 1; $i <= $count; $i++) {
+                $image_url = get_theme_mod("custom_review_image_$i");
+                $review_firstname = get_theme_mod("custom_review_firstname_$i");
+                $review_lastname = get_theme_mod("custom_review_lastname_$i");
+                $review_text = get_theme_mod("custom_review_text_$i");
+
+                // Zkontrolujeme, zda máme dostatečné informace o recenzi
+                if ($image_url && $review_firstname && $review_lastname && $review_text) {
+                ?>    
+                    <div class="review swiper-slide">
+                        
+                        <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($review_firstname . ' ' . $review_lastname); ?>">
+                        <div class="reviews_text">
+
+                            <header class="reviews-headline">
+                                
+                                <h3><?php echo wp_kses_post($review_firstname . ' <b class="accent-text">' . $review_lastname . '</b>'); ?></h3>
+                                
+                                <figure class="review-figure">
+                                    
+                                    <img class="reviews-approval" src="<?php echo get_template_directory_uri(); ?>/assets/img/recommend.png" alt="<?php echo esc_attr($review_firstname . ' ' . $review_lastname); ?> doporučuje Plus Fitness">
+                                    
+                                    <figcaption>Doporučuje Plus Fitness</figcaption>
+                                
+                                </figure>
+                            
+                            </header>
+                            
+                            <p><?php echo esc_html($review_text); ?></p>
+                        
+                        </div>
+                        
+                    </div>
+                
+                <?php
+                    }
+                }
+
+            ?>      
+                </div>
+                <div class="swiper-pagination reviews-pagination"></div>
+            </div>
+        </div>
+    </aside>
+    
+    
+    
+    <?php
+    return ob_get_clean();
+};
+
+add_shortcode('fitness_reviews', 'fitness_reviews_shortcode');
+
+/** gallery shortcode */
+function display_gallery() {
+    ob_start();
+    ?>
+    <section class="fitness-gallery" aria-label="Galerie obrázků prostorů našeho fitness">
+        
+        <h2 class="gallery-title">Galerie</h2>
+        
+        <div class="container">
+            
+            <div class="gallery-slider swiper">
+                
+                <div class="swiper-wrapper">
+                    <?php
+                    $gallery_count = get_theme_mod('fitness_gallery_count', 3);
+                    for ($i = 1; $i <= $gallery_count; $i++) {
+                        $image_url = get_theme_mod("fitness_gallery_image_$i");
+                        if ($image_url) {
+                            ?>
+                            <div class="gallery-slide swiper-slide">
+
+                                <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr__('Obrázek galerie', 'fitness') . ' ' . $i; ?>">
+                            
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+            
+            </div>
+            
+            <div class="swiper-button-next gallery-button-next"></div>
+            
+            <div class="swiper-button-prev gallery-button-prev"></div>
+        
+        </div>
+    </section>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('fitness_gallery', 'display_gallery');
